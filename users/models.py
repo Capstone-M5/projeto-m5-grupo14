cafc0e3d952a1d6c292be32d078d15e4b4befe0d
AlexFlavio <alexflavio.dev@gmail.com>
2022-10-31 17:01:57 -0300

    feature_UserModel: foi feito user model com uuid incluso

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=128)
+    ...
