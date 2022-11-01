5da9132ecbfffcdc5286055d3845a0a7d3b515fe
SusimaraRoberti <susimararoberti@outlook.com>
2022-11-01 16:51:17 -0300

    fix: Alterada model de User

---
+++ users/tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from users.models import User
+
+
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_data = {
+            "username": "ricardinho",
+            "name": "Ricardo Souza",
+            "password": "Ricardo10",
+            "email": "ricardo@outlook.com",
+        }
+
+        cls.user = User.objects.create_user(**cls.user_data)
+
+    def test_model_atribute(self):
+        user = User.objects.get(username="ricardinho")
+        name = user._meta.get_field("name").max_length
+        username = user._meta.get_field("username").max_length
+        email = user._meta.get_field("email").max_length
+
+        self.assertEqual(name, 128)
+        self.assertEqual(username, 128)
+        self.assertEqual(email, 128)
