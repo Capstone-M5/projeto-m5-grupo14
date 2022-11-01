bd5a100c9283e74477454512f7a7331999ee59e7
AlexFlavio <alexflavio.dev@gmail.com>
2022-11-01 16:37:36 -0300

    fix: Alterada model de User adicinando novas chaves para ficar de acordo com o DER

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from email.policy import default
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
@@ -7,4 +7,6 @@ import uuid
 class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=128)
+    username = models.CharField(max_length=128)
+    email = models.EmailField(unique=True, max_length=128)
     ...
