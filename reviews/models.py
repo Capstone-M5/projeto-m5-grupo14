9b4c960b74f5529eb05e27c30fb5d992cae50dff
SusimaraRoberti <susimararoberti@outlook.com>
2022-11-01 17:03:52 -0300

    feat:iniciado teste de model user

--- reviews/models.py
+++ reviews/models.py
@@ -3,14 +3,15 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 import uuid
 
+
 class Review(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    text =  models.TextField()
+    text = models.TextField(max_length=400)
     rating = models.PositiveIntegerField(
-                    validators=[
-                        MaxValueValidator(10),
-                        MinValueValidator(1)
-                    ])
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ])
     created_at = models.DateField(auto_now_add=True)
 
     user = models.ForeignKey(
@@ -20,4 +21,3 @@ class Review(models.Model):
         "videos.Video", on_delete=models.CASCADE, related_name="reviews"
     )
     ...
-
