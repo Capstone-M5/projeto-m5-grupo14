5589fff1f4c440f18475b870692758e9cced14b0
AlexFlavio <alexflavio.dev@gmail.com>
2022-11-01 16:40:46 -0300

    fix: Alterado propriedade unique em username para True

--- users/models.py
+++ users/models.py
@@ -7,6 +7,6 @@ import uuid
 class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=128)
-    username = models.CharField(max_length=128)
+    username = models.CharField(max_length=128, unique=True)
     email = models.EmailField(unique=True, max_length=128)
     ...
