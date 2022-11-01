2f972897f9b21a7a32721f51eabd6b39e0c201e2
AlexFlavio <alexflavio.dev@gmail.com>
2022-11-01 12:03:25 -0300

    fix: foi alterado na model de videos as chaves link e users

--- videos/models.py
+++ videos/models.py
@@ -6,9 +6,7 @@ class Video(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     title = models.CharField(max_length=128)
     thumbnail = models.CharField(max_length=128)
-    link = models.CharField(max_length=128)
+    link = models.CharField(max_length=128, unique=True)
     downloads = models.PositiveIntegerField(default=0)
-    users = models.ManyToManyField(
-        "users.User", on_delete=models.CASCADE, related_name="videos"
-    )
+    users = models.ManyToManyField("users.User", related_name="videos")
     ...
