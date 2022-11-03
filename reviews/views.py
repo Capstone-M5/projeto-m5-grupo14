295007c8c767e8297a2bf16dbaee21f945b1f390
renan-martini <renan.martiniduarte@gmail.com>
2022-11-03 11:42:43 -0300

    feat: view de edição e deleção de reviews concluida

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from .models import Review
+from .serializers import ReviewSerializer
+from .permissions import IsAdmin_Or_ReviewOwner
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+
+class RetrieveUpdateDestroyReviewView(RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAdmin_Or_ReviewOwner]
