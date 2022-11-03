90ad972997e5260544f1e7c21d70201af0e72405
SusimaraRoberti <susimararoberti@outlook.com>
2022-11-03 18:22:30 -0300

    feat:testado view de criaçãom edição e deleção de reviews e corrigido permissions

--- reviews/permissions.py
+++ reviews/permissions.py
@@ -4,8 +4,8 @@ from rest_framework import permissions
 class IsAdmin_Or_ReviewOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in ("PATCH", "GET"):
-            return bool(request.user and request.user.is_authenticated and obj.username == request.user.username)
+            return bool(request.user and request.user.is_authenticated and obj.user.username == request.user.username)
         return bool(
-            (request.user and request.user.is_authenticated and obj.username ==
+            (request.user and request.user.is_authenticated and obj.user.username ==
              request.user.username) or request.user.is_superuser
         )
