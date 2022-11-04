63af3ba99f3859f0f10715fbdda9ed6937dc29e2
SusimaraRoberti <susimararoberti@outlook.com>
2022-11-04 11:06:07 -0300

    feat:preparando ambiente para o deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn psytube.wsgi
--- psytube/settings.py
+++ psytube/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rg(362w069m#qrvd5nkat(bxgx+xgp(5e_vmjaw@ppl0ob(-xo"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
